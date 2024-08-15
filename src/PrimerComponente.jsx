0e89c31abeb15d560f9413b3c2724ac60034e405
Camilo0291 <acquintanam@gmail.com>
2024-08-15 10:21:27 -0500

    fistr

---
+++ src/PrimerComponente.jsx
@@ -0,0 +1,14 @@
+import logo from './assets/logo.png'
+
+const PrimerComponente = () => {
+
+    return (
+        <header className="header">
+            <img src={logo} alt="Imagen" />
+            <a href="">Enlace</a>
+            <h1>Primer Componente</h1>
+        </header>
+    );
+};
+
+export default PrimerComponente;
